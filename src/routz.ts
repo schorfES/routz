2082a86f83c950184f546931baa1b447b2a32263
Norman Rusch <webmaster@grind-it.de>
2021-08-10 13:15:46 +0200

    Change doctype defintions

--- src/routz.ts
+++ src/routz.ts
@@ -2,40 +2,30 @@ export type RouteDefinition = string;
 
 export type Route = string;
 
-export type Routes = Record<string, RouteDefinition>;
+export type Routes = Record<Route, RouteDefinition>;
 
 export type Params = Record<string, string> | null;
 
-/**
- * Returns the route defintion by a name.
- * @private
- * @throws {Error} when given name is not found in routes.
- */
 export type Receiver = (name: string) => RouteDefinition;
 
-/**
- * Builds a url for a route by name and params. Params are dynamic parts of a
- * url that are defined in the format `[param]`. Optional params are defined by
- * a question mark at the end like `[optionalParam?]`. The params need to be
- * passed as a key/value object, optional params doesn't need to be defined in
- * this object while missing regular params will throw an error.
- *
- * @throws {Error} when given name is not found in routes.
- * @throws {Error} when given params are not matching the route definition.
- */
 export type Resolver = (name: string, params?: Params) => Route;
 
-/**
- * Defines the routes and returns scoped `recieve` and `resolve` functions that
- * access the defined routes.
- */
 export type Definition = {
 	receive: Receiver;
 	resolve: Resolver;
 };
 
-export const define = (routes: Routes): Definition => {
+/**
+ * Defines the routes and returns scoped `recieve` and `resolve` functions that
+ * access the defined routes.
+ */
+export function define(routes: Routes): Definition {
 
+	/**
+	 * Returns the route defintion by a name.
+	 *
+	 * @throws {Error} when given name is not found in routes.
+	 */
 	const receive: Receiver = (name) => {
 		const route = routes[name];
 		if (!route) {
@@ -45,6 +35,16 @@ export const define = (routes: Routes): Definition => {
 		return route;
 	};
 
+	/**
+	 * Builds a url for a route by name and params. Params are dynamic parts of a
+	 * url that are defined in the format `[param]`. Optional params are defined by
+	 * a question mark at the end like `[optionalParam?]`. The params need to be
+	 * passed as a key/value object, optional params doesn't need to be defined in
+	 * this object while missing regular params will throw an error.
+	 *
+	 * @throws {Error} when given name is not found in routes.
+	 * @throws {Error} when given params are not matching the route definition.
+	 */
 	const resolve: Resolver = (name, params) => {
 		const REGEXP_PARAMS = /\[([a-zA-Z]*)(\??)\]/g;
 		const REGEXP_SLASH_SANITIZED = /\/(\/*)/g;
@@ -86,8 +86,5 @@ export const define = (routes: Routes): Definition => {
 		return path;
 	};
 
-	return {
-		receive,
-		resolve,
-	};
-};
+	return { receive, resolve };
+}
